90b857246c7740acbf1e8f443406754f4457917b
Avneet Singh Malhotra <avneetmalhotra02@gmail.com>
2017-09-13 13:10:45 +0530

    Add files via upload
    Exercise 2.2 Traversing
    sandbox.js updated

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  
+/*** Exercise:2.1 Selecting ***/
+
   //1.
   $(".module");
 
@@ -24,4 +27,24 @@ $(document).ready(function(){
   
   //6.
   $("tbody tr:nth-child(odd)");
+
+
+/****************************************/
+/*** Exercise:2.2 Traversing ***/
+
+  //1.Select all of the image elements on the page; log each image's alt attribute.
+  console.log($("img[alt]"));
+
+  //2.Select the search input text box, then traverse up to the form and add a class to the form.
+  $(".input_text").parent().addClass("search-form");
+
+  //3.Select the list item inside #myList that has a class of "current" and remove that class from it; add a class of "current" to the next list item.
+  $("#myList .current").removeClass('current').next().addClass('current');
+
+  //4.Select the select element inside #specials; traverse your way to the submit button.
+  $("#specials").find('input[type="submit"');
+
+  //5.Select the first list item in the #slideshow element; add the class "current" to it, and then add a class of "disabled" to its sibling elements.
+  $("#slideshow li:first").addClass('current').next().addClass('disabled');
+
 });
