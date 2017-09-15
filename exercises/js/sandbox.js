db0c1d7016bc5098c848aff832347fd59f705f7c
Avneet Singh Malhotra <avneetmalhotra02@gmail.com>
2017-09-15 14:00:43 +0530

    Exercise 2.2 Traversing - submission 3
    sandbox.js updated as asked.

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
   $("#myList li:nth-child(3)");
   $("#listItem_2 + li");
   //Which is the best to use? Why?
-  //ans- Id selector is bect to use because it is unique and so fastest
+  //ans- Id selector is best to use because it is unique and so fastest
 
   //3.Select the label for the search input using an attribute selector.
   $("label[for='q']");
@@ -31,23 +31,21 @@ $(document).ready(function(){
 /*** Exercise:2.2 Traversing ***/
 
   //1.Select all of the image elements on the page; log each image's alt attribute.
-  $('img[alt]').each(function(index, imgElem){
-    console.log('Image: ' + index + ', alt: ' + imgElem.alt);
-  })
+  $('img').attr('alt', function(index, altValue){
+    console.log('Image: ' + index + ', alt: ' + altValue);
+  });
 
   //2.Select the search input text box, then traverse up to the form and add a class to the form.
   $(".input_text").parent().addClass("search-form");
 
   //3.Select the list item inside #myList that has a class of "current" and remove that class from it; add a class of "current" to the next list item.
-  $("#myList .current").removeClass('current').next().addClass('current');
+  $("#myList li.current").removeClass('current').next().addClass('current');
 
   //4.Select the select element inside #specials; traverse your way to the submit button.
-  var selectDayElement = $('#specials').find('select').filter('[name="day"]'); 
-  var submitDayButton = selectDayElement.parent().next().find('input').filter('[type="submit"]');
-  console.log(submitDayButton);
+  $('#specials select').closest('form').find('input[type="submit"]');
 
   //5.Select the first list item in the #slideshow element; add the class "current" to it, and then add a class of "disabled" to its sibling elements.
-  $("#slideshow li:first").addClass('current').next().addClass('disabled');
+  $("#slideshow li:first").addClass('current').nextAll().addClass('disabled');
 
 
 /****************************************/
