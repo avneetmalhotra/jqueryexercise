575c488d4074ad59ef6a9f2affd9de0aa79e171c
Avneet Singh Malhotra <avneetmalhotra02@gmail.com>
2017-09-18 12:21:22 +0530

    Exercise 2.2 - submission 4
    sandbox.js updated

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -32,8 +32,8 @@ $(document).ready(function(){
 
   //1.Select all of the image elements on the page; log each image's alt attribute.
   $('img').each(function(index, imgElem){
-    imgObj = $(imgElem);
-    console.log('Image: ' + index + ', alt: ' +imgObj.attr('alt'));
+    var $imgObj = $(imgElem);
+    console.log('Image: ' + index + ', alt: ' +$imgObj.attr('alt'));
   });
 
   //2.Select the search input text box, then traverse up to the form and add a class to the form.
@@ -47,6 +47,8 @@ $(document).ready(function(){
 
   //5.Select the first list item in the #slideshow element; add the class "current" to it, and then add a class of "disabled" to its sibling elements.
   $("#slideshow li:first").addClass('current').siblings().addClass('disabled');
+
+
 /****************************************/
 /*** Exercise:2.3 Manipulating ***/
 
