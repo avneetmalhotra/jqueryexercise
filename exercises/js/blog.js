36cecdc6333dee5114c70ad493b21e03d5bd41b6
Avneet Singh Malhotra <avneetmalhotra02@gmail.com>
2017-09-22 14:22:35 +0530

    Exercise 5.1 - submission 2
    blog.js updated to follow oop approach

--- exercises/js/blog.js
+++ exercises/js/blog.js
@@ -1,18 +1,31 @@
-/************************/
 /*Exercise 5.1 Reveal hidden text*/
+function Blog(selector){
+  this.$blog = $(selector.blog);
+  this.$headlinesAnchors = this.$blog.find('a');
+}
 
-$(document).ready(function(){
-  $('#blog').find('a').click(function(event){
-    var anchorHeadline = $(this);
+Blog.prototype.init = function(){
+  this.$headlinesAnchors.click(this.headlinesAnchorClickHandler());
+};
+
+Blog.prototype.headlinesAnchorClickHandler = function(){
+  var that = this;
+
+  return function(event){
     event.preventDefault();
     
-    var excerptPara = anchorHeadline.closest('h3').siblings('p.excerpt');
-    if(excerptPara.is(':hidden')){
-      $('#blog p.excerpt').slideUp();
-      excerptPara.slideDown();
+    var $excerptPara = $(this).closest('h3').siblings('p.excerpt');
+    
+    if($excerptPara.is(':hidden')){
+      that.$blog.find('p.excerpt').slideUp();
+      $excerptPara.slideDown();
     }
-    else{
-      excerptPara.slideUp();
-    }
-  });
+    else
+      $excerptPara.slideUp();
+  };
+};
+
+$(document).ready(function(){
+  var blog = new Blog({'blog' : '#blog'});
+  blog.init();
 });
