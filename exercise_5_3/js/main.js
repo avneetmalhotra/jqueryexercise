e5a6ee8d2fd9313abbe51c9819db5b2a6b56776b
Avneet Singh Malhotra <avneetmalhotra02@gmail.com>
2017-09-22 16:48:16 +0530

    Delete main.js

--- exercise_5_3/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  var slideshow = new Slideshow($('#slideshow')),
-      slideshowNavbar = new Navbar(slideshow, 'slideshowNav');
-  
-  slideshow.init();
-  slideshowNavbar.init();
-  
-  slideshow.showSlideshow(slideshowNavbar);
-});
