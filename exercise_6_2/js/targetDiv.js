264fd8c92cc5bf07f6dd8b4737fabf60f1525475
Avneet Singh Malhotra <avneetmalhotra02@gmail.com>
2017-10-04 13:49:01 +0530

    Delete targetDiv.js

--- exercise_6_2/js/targetDiv.js
+++
@@ -1,9 +0,0 @@
-//TargetDiv class
-function TargetDiv(parentSelector){
-  this.parentSelector = parentSelector;
-  this.specialsForm = $(parentSelector).find('form');
-}
-
-TargetDiv.prototype.createTargetDiv = function(){
-  this.$targetDivCreated = $('<div class="target"></div>').insertAfter(this.specialsForm);
-};
